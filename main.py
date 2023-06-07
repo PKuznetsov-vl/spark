b1b6d6f57d434c695dadee313bcd119e5a0ea7ce
pavel <ponch092009@yandex.ru>
2023-06-07 23:16:33 +0300

    init

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ import spark_parse
 # IsActual:Any,EntityType:Any}&bounds=&documentType=None&ObjectType=Search
 
 if __name__ == '__main__':
-    #example
+    # example
     a = spark_parse.Spark()
     inn_list = [7710699964, 7707083893, 2310031475, 5036045205, ]
     for inn in inn_list:
         a.company_inn = inn
 
         print(a.accountant_report())
-
