7fe366db9cab1ff8603a20de4432e1f126723803
Vladimir Zabolotskikh <m30b34735@gmail.com>
2023-07-01 19:42:25 +0300

    Add Get_owners

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ def get_guid(inn):
 if __name__ == '__main__':
     # example
     a = spark_parse.Spark()
-    inn_list = [7707083893,5032339290,9731092941,7706107510]
-    for inn in inn_list:
-        a.company_inn = inn
-        print(a.get_cash_flow())
-        print(a.get_fin_report())
-        print(a.get_balance_report())
-        print(a.accountant_report())
-        #df = pd.read_excel( a.get_xlsx())
-        #print(df.head(10))
+    # inn_list = [7707083893,5032339290,9731092941,7706107510]
+    # for inn in inn_list:
+    #     a.company_inn = inn
+    #     print(a.get_cash_flow())
+    #     print(a.get_fin_report())
+    #     print(a.get_balance_report())
+    #     print(a.accountant_report())
+    #     #df = pd.read_excel( a.get_xlsx())
+    #     #print(df.head(10))
+
+    a.company_inn =9731092941
+    guid = a.get_guid(a.company_inn)
+    print(guid)
+    print(a.get_owners(guid))
+
 
--- spark_parse.py
+++ spark_parse.py
@@ -1,5 +1,6 @@
 import base64
 import io
+import time
 from functools import cached_property, lru_cache
 import atexit
 from typing import Any
@@ -7,24 +8,27 @@ import logging
 import requests
 import warnings
 from PIL import Image
+import operator
+from dataclasses import dataclass
 
 warnings.simplefilter("ignore")
 
 
 class Spark:
-    __slots__ = ("__username", "__password", "company_inn", "sess", "logger")
+    __slots__ = ("__username", "__password", "company_inn", "sess", "logger", "__host")
 
     def __init__(self):
         """
                 Initialize the spark object with a session and pass and login.
         """
         self.__username = 'skoltech8'
-        self.__password = 'dEB-hF9-Kzu-W37'
+        self.__password = 'bdD4$#AeT5uA?K~'
         self.company_inn = None
         atexit.register(self.logout)
         self.sess = requests.Session()
         self.logger = logging.getLogger(__name__)
         logging.basicConfig(filename='spark_logs.txt', level=logging.INFO)
+        self.__host = "https://spark-interfax.ru"
 
         login_response = self.__login()
         if login_response.status_code == 401:
@@ -39,7 +43,7 @@ class Spark:
 
     def __login(self, captcha='', user_captcha=''):
         login_response = self.sess.post('https://spark-interfax.ru/system/sapi/auth/credentials?format=json&s_up=ssl',
-                                        files={'UserName': (None, 'skoltech8'), 'Password': (None, 'dEB-hF9-Kzu-W37'),
+                                        files={'UserName': (None, self.__username), 'Password': (None, self.__password),
                                                'RememberMe': (None, 'false'), 'Captcha': (None, captcha),
                                                'UserCaptcha': (None, user_captcha)}, verify=False)
 
@@ -80,6 +84,7 @@ class Spark:
             return None
 
     # 7710699964 CDF0F6BA74A94D8EBD174BD9C10B8491
+
     def get_company_info(self) -> str:
         """Краткое инфо о компании
             :return: json object"""
@@ -191,24 +196,25 @@ class Spark:
             # print(f"Error: {e}")
             self.logger.error(f"get_shareholders failed for company inn:{self.company_inn} Error:{e} ")
             return None
-    def get_pledges(self)-> str | None:
+
+    def get_pledges(self) -> str | None:
         """Информация о залогах
             Returns: json object"""
         try:
             pledges = self.sess.get(f"https://spark-interfax.ru/sapi/pledges/summary?CompanyKey="
-                                     f"%7BCompanyGuid%3A{self.get_guid(self.company_inn)}%7D")
+                                    f"%7BCompanyGuid%3A{self.get_guid(self.company_inn)}%7D")
             pledges.raise_for_status()
             return pledges.json()
         except requests.exceptions.HTTPError as e:
             self.logger.error(f"get_pledges failed for company inn:{self.company_inn} Error:{e} ")
             return None
 
-    def get_intellectual_property(self)-> str | None:
+    def get_intellectual_property(self) -> str | None:
         """Информация о интеллектуальной собственности
             Returns: json object"""
         try:
             iproperty = self.sess.get(f"https://spark-interfax.ru/sapi/intellectualproperty?CompanyKey="
-                                     f"%7BCompanyGuid%3A{self.get_guid(self.company_inn)}%7D")
+                                      f"%7BCompanyGuid%3A{self.get_guid(self.company_inn)}%7D")
             iproperty.raise_for_status()
             return iproperty.json()
         except requests.exceptions.HTTPError as e:
@@ -216,13 +222,12 @@ class Spark:
             self.logger.error(f"get_intellectual_property failed for company inn:{self.company_inn} Error:{e}")
             return None
 
-
-    def get_graphs(self)-> str | None:
+    def get_graphs(self) -> str | None:
         """График в виде JSON объекта
             Returns: json object"""
         try:
             graph = self.sess.get(f"https://spark-interfax.ru/sapi/finanalysisbankgraphs?CompanyKey="
-                                     f"%7BCompanyGuid%3A{self.get_guid(self.company_inn)}%7D")
+                                  f"%7BCompanyGuid%3A{self.get_guid(self.company_inn)}%7D")
             graph.raise_for_status()
             return graph.json()
         except requests.exceptions.HTTPError as e:
@@ -230,19 +235,172 @@ class Spark:
             self.logger.error(f"get_graphs failed for company inn:{self.company_inn} Error:{e}")
             return None
 
-    def get_coefficients(self)-> str | None:
+    def get_coefficients(self) -> str | None:
         try:
             coefficients = self.sess.get(f"https://spark-interfax.ru/sapi/coefficients/bank?CompanyKey="
-                                     f"%7BCompanyGuid%3A{self.get_guid(self.company_inn)}%7D")
+                                         f"%7BCompanyGuid%3A{self.get_guid(self.company_inn)}%7D")
             coefficients.raise_for_status()
             return coefficients.json()
         except requests.exceptions.HTTPError as e:
             # print(f"Error: {e}")
             self.logger.error(f"get_coefficients failed for company inn:{self.company_inn} Error:{e}")
             return None
-    # TODO cвязи
-    # https://spark-interfax.ru/system/sapi/graph/s3/OwnershipAnalysis?jsconfig=eccn%2Ceti
-    # TODO неуверенный поиск
-    # https://spark-interfax.ru/system/sapi/companylist/search?&pageSize=30&pageNo=1&query=
-    # {"Unknown":"сбер"}&type=Unknown&filter={Country:RUS,OkvedCodes:[],CompanySize:Unknown,RegionCodes:[],
-    # IsActual:Any,EntityType:Any}&bounds=&documentType=None&ObjectType=Search
+
+    def __get_json(self, postfix, headers, params):
+        """Do api reqest and get json response
+        Сделать Api запрос, получить json ответ"""
+
+        time.sleep(3)
+        try:
+            response = self.sess.get(f"{self.__host}{postfix}", params=params, headers=headers)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.HTTPError as e:
+            self.logger.error(f"Request failed for postfix:{postfix},"
+                              f" CompanyKey:{params} Error:{e}")
+            return None
+
+    def get_available_methods(self, guid):
+        """
+        Get avalibale for this company methods
+        Получить доступные для данной компании методы(совладельцы анализ владения и тд)
+
+        """
+        postfix = "/sapi/card/menu"
+        reqest_params = {
+            "Reset": False,
+            "CompanyKey": "{{CompanyGuid:{0}}}".format(guid)
+        }
+        reqest_headers = {
+            "Accept": "application / json",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "ru"
+        }
+        response = self.__get_json(postfix=postfix, headers=reqest_headers, params=reqest_params)
+        if response != None:
+            methods = []
+            for section in response:
+                for method in section:
+                    methods.append(method["Name"])
+
+        else:
+            methods = None
+
+        return methods
+
+    def __get_owners_vision(self, guid):
+        """
+        Узнать видимы ли совладельцы компании
+        """
+        reqest_params = {
+            "CompanyKey": "{{CompanyGuid:{0}}}".format(guid)
+        }
+        print(reqest_params)
+        reqest_headers = {
+            "Accept": "application / json",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "ru"
+        }
+        postfix = "/sapi/card/shareholders/egrul/hidden"
+        vision = self.__get_json(postfix=postfix, headers=reqest_headers, params=reqest_params)
+        print(vision)
+        if vision is not None:
+            visible = vision['IsHidden']
+        else:
+            visible = None
+        return visible
+
+    def __get_ownership_periods_id(self, guid):
+        """
+        Получить информацию о периодах владения компанией,
+
+        """
+        postfix = "/sapi/card/shareholders/disclosure/periods"
+        reqest_params = {
+            "CompanyKey": "{{CompanyGuid:{0}}}".format(guid)
+        }
+        reqest_headers = {
+            "Accept": "application / json",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "ru"
+        }
+        period_response = self.__get_json(postfix=postfix, headers=reqest_headers, params=reqest_params)
+
+        if period_response != None:
+            periods = period_response['Periods']
+        else:
+            periods = None
+        return periods
+
+    def __get_ownership_period_info(self, guid, last_period):
+        """Получить информацию о владельцах за временной период
+
+        """
+        postfix = "/sapi/card/shareholders/disclosure/list"
+        reqest_params = {
+            "CompanyKey": "{{CompanyGuid:{0}}}".format(guid),
+            "PeriodId": last_period
+        }
+        reqest_headers = {
+            "Accept": "application / json",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "ru"
+        }
+        owners = self.__get_json(postfix=postfix, headers=reqest_headers, params=reqest_params)
+        if owners is not None:
+            output = owners
+        else:
+            output = None
+        return output
+
+    def get_owners(self, guid):
+        """
+        Получить информформацию о совладельцах компании
+        По умолчанию возвращает данные о самом свежем оступном
+         периоде владения
+        """
+        hidden = self.__get_owners_vision(guid)
+        try:
+            if hidden is False:
+                periods = self.__get_ownership_periods_id(guid)
+                own_periods = [period for period in periods if period['Type'] == 'Own']
+                if len(own_periods) == 0:
+                    owners = self.__get_current_owners(guid)
+
+                else:
+                    last_period = own_periods[-1]['Id']
+                    owners = self.__get_ownership_period_info(guid, last_period)
+
+            else:
+                owners = ["Hidden"]
+
+            return owners
+        except TypeError as e:
+            self.logger.error(f"get_owners failed for company:{guid},Error:{e}")
+            return None
+
+    def __get_current_owners(self, guid):
+        """Получить информацию о текущих владельцах"""
+
+        postfix = "/sapi/card/shareholders/egrul/current"
+        reqest_params = {
+            "CompanyKey": "{{CompanyGuid:{0}}}".format(guid),
+        }
+        reqest_headers = {
+            "Accept": "application / json",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "ru"
+        }
+        owners = self.__get_json(postfix=postfix, headers=reqest_headers, params=reqest_params)
+        if owners is None:
+            output = owners
+        else:
+            output = None
+        return output
+
+# TODO cвязи
+# https://spark-interfax.ru/system/sapi/graph/s3/OwnershipAnalysis?jsconfig=eccn%2Ceti
+# TODO неуверенный поиск
+# https://spark-interfax.ru/system/sapi/companylist/search?&pageSize=30&pageNo=1&query=
+# {"Unknown":"сбер"}&type=Unknown&filter={Country:RUS,OkvedCodes:[],CompanySize:Unknown,RegionCodes:[],
+# IsActual:Any,EntityType:Any}&bounds=&documentType=None&ObjectType=Search
